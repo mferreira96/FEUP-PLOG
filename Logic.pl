1dd9aacdccdae432b99c76ba4673022d5024d7ee
mferreira96 <pount@sapo.pt>
2016-10-17 16:52:48 +0100

    added file Logic.pl

---
+++ Logic.pl
@@ -0,0 +1,11 @@
+moveAdaptoid(Board, Player, ListInitialCoords, ListFinalCoords, NewBoard).
+
+addLeg(Board, Player, Coords, NewBoard).
+
+addPincer(Board, Player, Coords, NewBoard).
+
+getAdaptoid(Board, Player, Coords, Adaptoid).
+
+removeAdaptoid(Board, Player, Coords, NewBoard).
+
+putAdaptoid(Board, Player, Coords, NewBoard).
