fbd8e0d2884f4c5d12952c5c82dd053af9816e95
mferreira96 <pount@sapo.pt>
2016-10-19 21:27:39 +0100

    findElement created

--- Logic.pl
+++ Logic.pl
@@ -4,7 +4,7 @@ addLeg(Board, Player, Coords, NewBoard).
 
 addPincer(Board, Player, Coords, NewBoard).
 
-getAdaptoid(Board, Player, Coords, Adaptoid).
+getAdaptoid(Board, Player, CoordX, CoordY, Adaptoid).
 
 removeAdaptoid(Board, Player, Coords, NewBoard).
 
@@ -13,3 +13,20 @@ putAdaptoid(Board, Player, Coords, NewBoard).
 editCell(Board, Remove, Put, Coords, NewBoard).
 
 updateAdaptoid(Adaptoid, NewAdaptoid, pincer, leg).
+
+
+%%% listManipulation %%%
+%%% This Way we can find our adaptoid ont the required position, this predicate allow us to search verticaly and horizontaly %%%
+
+findElement(PosXY, [Head|Rest], Result, Counter):-
+  PosXY =/= Counter,
+  Counter1 is Counter + 1,
+  findElement(PosXY, Rest, list, Counter1).
+
+findElement(PosXY, [Head|Rest], Result ,Counter):-
+    CoordX =:= Counter,
+    List = Head.
+
+getAdaptoid(Board, Player, CoordX, CoordY, Adaptoid):-
+    findElement(CoordX, Board, List, 1),
+    findElement(CoordY, List, Adaptoid, 1).
