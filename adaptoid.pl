b250070bc7940308e486d9f619a5bff20b67cc6b
mferreira96 <pount@sapo.pt>
2016-10-27 11:30:57 +0100

    added interface.pl

--- adaptoid.pl
+++ adaptoid.pl
@@ -5,5 +5,8 @@ adaptoid:-
   tabuleiro(_X),
   %% displayBoard(_X, 0),
   moveAdaptoid(_X, _ , 3 - 1, 1 - 5, Y),
-removeAdaptoid(Y, _ , 1 - 5, T),
+  removeAdaptoid(Y, _ , 1 - 5, T),
   displayBoard(T, 0).
+
+
+% changePlayer() -> 
