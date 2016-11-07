741bdf5541a24647a6324a0e4eccf5c6067d85c7
mferreira96 <pount@sapo.pt>
2016-11-07 19:16:07 +0000

    working on interface

--- Adaptoid/src/interface.pl
+++ Adaptoid/src/interface.pl
@@ -1,17 +1,52 @@
 
-% needed to be tested
-askCoord:-
-  write('Write the row of the adaptoid:'),
-  getInt(Row),
-  validateRow(Row),
-  write('Write the column of the adaptoid:'),
-  getInt(column),
-  validateColumn(Column),
-  \+empetyCell(Board, _ , Row-Column).
+startMenu:- clearScreen(20),
+  write('Choose one of the follow options'), nl,
+  write('1 - Play'), nl,
+  write('2 - Tuturial'),nl,
+  write('3 - About'), nl,
+  write('4 - Exit'),nl,
+  nl,
+  write('Press Enter to continue'),
+  get_char(Answer), discardInput.
+
+%  fazer o switch case aqui
+
+gameOption:- clearScreen(20),
+  write('Choose one of the follow options'), nl,
+  write('1 - Computer vs Computer'), nl,
+  write('2 - Computer vs Human'),nl,
+  write('3 - Human vs Human'), nl,
+  write('4 - Exit'),nl,
+  nl,
+  write('Press Enter to continue'),
+  get_char(Answer), discardInput.
 
 
-askNextCoords:-
-  write('write the next position of adaptoid:').
+chooseComputerLevel:- clearScreen(20),
+  write('Choose one of the follow options'), nl,
+  write('1 - Hard'), nl,
+  write('2 - Medium'),nl,
+  write('3 - Exit'),nl,
+  nl,
+  write('Press Enter to continue'),
+  get_char(Answer), discardInput.
+
+choose2ComputerLevel:- clearScreen(20),
+  write('Choose one of the follow options'), nl,
+  write('1 - Hard vs Hard'), nl,
+  write('2 - Hard vs Medum'),nl,
+  write('3 - Medium vs Medum'),nl,
+  write('4 - Exit'),nl,
+  nl,
+  write('Press Enter to continue'),
+  get_char(Answer), discardInput.
+
 
 discardInput:-
   get_code(_).
+
+clearScreen(0).
+clearScreen(N):-
+  nl,
+  N1 is N - 1,
+  clearScreen(N1).
