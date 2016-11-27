0be18a0d4fec05113b763f46c4274058ac491cdb
mferreira96 <pount@sapo.pt>
2016-11-27 18:35:29 +0000

    creating a filled Board

--- Clouds/board.pl
+++ Clouds/board.pl
@@ -1,5 +1,11 @@
 :- use_module(library(lists)).
 
+translate(vazio, ' ').
+translate(cheio, 'X').
+
+
+
+
 displayTop(0) :- nl.
 displayTop(Counter) :-
       Counter > 0,
@@ -16,10 +22,29 @@ displayMid(Counter) :-
 
 displayBoard(0,Tamanho):-displayTop(Tamanho).
 displayBoard(N,Tamanho):-
-        N1 is N-1,
-        Mid is Tamanho+1,
-        displayTop(Tamanho),
-        displayMid(Mid),
-        displayMid(Mid),
-        displayBoard(N1,Tamanho).
-        
+      N1 is N-1,
+      Mid is Tamanho+1,
+      displayTop(Tamanho),
+      displayMid(Mid),
+      displayMid(Mid),
+      displayBoard(N1,Tamanho).
+
+fillList(0,_,[]).
+fillList(N,X,[X|Xs]) :-
+      N > 0,
+      N1 is N-1,
+      fillList(N1,X,Xs).
+
+createBoard(_, 0,[]).
+createBoard(Tamanho,Counter,[X|Xs]):-
+    Counter > 0,
+    Counter1 is Counter - 1,
+    fillList(Tamanho,vazio,X),
+    createBoard(Tamanho, Counter1, Xs, NewBoard).
+
+    // melhor utilizar append
+
+
+teste(Tamanho):-
+    createBoard(Tamanho, Tamanho, Board),
+    format('~w ', [Board]), nl.
