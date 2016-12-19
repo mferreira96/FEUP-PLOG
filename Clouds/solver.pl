912bffe17459e4148af098e2ee76706db38e43f0
mferreira96 <pount@sapo.pt>
2016-12-19 12:50:00 +0000

    getBoardElement fixed

--- Clouds/solver.pl
+++ Clouds/solver.pl
@@ -31,11 +31,10 @@ solver(Line_Clues, Column_Clues, SolvedBoard):-
   % -> should have a white space between clouds
 
 %Get an element based on coords from a 2d list
-getBoardElem(1-Col,[H|_],Color):- nth0(Col,H,Color).
-getBoardElem(Line-Col,[_|Hs],Color):-
-        Line > 1,
-        Line1 is Line - 1,
-        getBoardElem(Line1-Col,Hs,Color).
+getBoardElem(LineIndex-ColIndex,Matrix,Color):-
+   nth0(LineIndex, Matrix, Line)
+   nth0(ColIndex,Line,Color).
+
 
 
 
